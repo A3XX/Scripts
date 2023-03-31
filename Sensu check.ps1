a725f249a9568718bce27d87ce68352a0fa0e643
A3XX <69278404+A3XX@users.noreply.github.com>
2023-03-31 12:30:34 -0400

    Add files via upload

---
+++ Sensu check.ps1
@@ -0,0 +1,14 @@
+﻿$computers = Get-ADComputer -Filter {Enabled -eq $true} | Select-Object -ExpandProperty Name
+
+foreach ($computer in $computers) {
+    if (Test-WSMan -ComputerName $computer -ErrorAction SilentlyContinue) {
+        $service = Get-Service -ComputerName $computer -Name "sensu"
+        if ($service.Status -eq "Running") {
+            Write-Host "The Sensu service is running on $computer."
+        } else {
+            Write-Host "The Sensu service is not running on $computer."
+        }
+    } else {
+        Write-Host "Could not connect to $computer."
+    }
+}
